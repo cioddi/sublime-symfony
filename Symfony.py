eb7edf0583d294d359afc48bfc61ee200d5410ee
Max Tobias Weber <maxtobiasweber@gmail.com>
2013-07-18 16:24:01 +0200

    Update Symfony.py
    make it work on *nix environments
    hope it does not break anything on windows

--- Symfony.py
+++ Symfony.py
@@ -1,4 +1,4 @@
-import sublime, sublime_plugin, re
+import sublime, sublime_plugin, re, os
 
 wait_file = False
 show_action = ''
@@ -6,8 +6,19 @@ class SymfonyViewCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		global wait_file
 		global show_action
+		
+		if os.sep == '/':
+			pathsep = '/'
+			regex_pathsep = '/'
+		elif os.sep == '\\':
+			pathsep = '\\'
+			regex_pathsep = '\\\\'
+
+
 		file = self.view.file_name()
-		file_re = re.search('(.+)\\\\(.+)Controller.php', file)
+		file_re = re.search('(.+)' + regex_pathsep + '(.+)Controller.php', file)
+
+
 		if file_re:
 			folder = file_re.group(1)
 			file = file_re.group(2)
@@ -16,13 +27,13 @@ class SymfonyViewCommand(sublime_plugin.TextCommand):
 				line = self.view.word(action)
 				line_contents = self.view.substr(line)
 				a = re.search('(\w+)Action', line_contents).group(1)
-				self.view.window().open_file(folder+'\\..\\Resources\\views\\'+file+'\\'+a+'.html.twig')
-		elif re.search('(.+)\\\\(.+).html.twig', file):
-			file_re = re.search('(.+)\\\\(.+)\\\\(.+).html.twig', file)
+				self.view.window().open_file(folder + pathsep + '..' + pathsep + 'Resources' + pathsep + 'views' + pathsep + file + pathsep + a + '.html.twig')
+		elif re.search('(.+)' + regex_pathsep + '(.+).html.twig', file):
+			file_re = re.search('(.+)' + regex_pathsep + '(.+)' + regex_pathsep + '(.+).html.twig', file)
 			folder = file_re.group(1)
 			controller = file_re.group(2)
 			action = file_re.group(3)
-			view = self.view.window().open_file(folder+'\\..\\..\\Controller\\'+controller+'Controller.php')
+			view = self.view.window().open_file(folder + pathsep + '..' + pathsep + '..' + pathsep + 'Controller' + pathsep + controller+'Controller.php')
 			wait_file = True
 			show_action = action
 		else:
